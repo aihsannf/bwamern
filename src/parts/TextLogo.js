2a65e8c0c5778fbbf86817e0c05c3b7a5677bdcd
Ahmad Ihsan Nur Fata <aihsannf07@gmail.com>
2020-08-29 21:21:40 +0700

    Create header component and put it to the LandingPage

---
+++ src/parts/TextLogo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function TextLogo() {
+  return (
+    <div>
+      <Button className="brand-logo" href="" type="link">
+        Stay<span className="text-gray-900">cation.</span>
+      </Button>
+    </div>
+  );
+}
