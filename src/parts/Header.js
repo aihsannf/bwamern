b19a62cbc2d1aea9b3ba90539de5a639f46d6941
Ihsan <aihsannf07@gmail.com>
2020-09-04 14:35:01 +0700

    Adding react-reveal and a bit of landingpage animation

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Button from "elements/Button";
 import TextLogo from "parts/TextLogo";
 
@@ -8,36 +9,38 @@ export default function Header(props) {
   };
 
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <TextLogo />
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className={`nav-item${getNavLinkCLass("/")}`}>
-                <Button className="nav-link" type="link" href="/">
-                  Home
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkCLass("/browse-by")}`}>
-                <Button className="nav-link" type="link" href="/browse-by">
-                  Browse by
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkCLass("/stories")}`}>
-                <Button className="nav-link" type="link" href="/stories">
-                  Stories
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkCLass("/agents")}`}>
-                <Button className="nav-link" type="link" href="/agents">
-                  Agents
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <TextLogo />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkCLass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkCLass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">
+                    Browse by
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkCLass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="/stories">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkCLass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="/agents">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 }
